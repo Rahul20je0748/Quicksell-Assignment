5bd20472fa1ba94885fce762f79ff33cd4242678
RAHUL KUMAR GUPTA <97753238+Rahul20je0748@users.noreply.github.com>
2023-10-15 00:41:02 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import KanbanBoard from './components/KanbanBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
